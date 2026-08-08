be2ee50bd18241bba02dbb5196dc2a85ff7460e9
Delia-Dwi-Anjani <84898521+Delia-Dwi-Anjani@users.noreply.github.com>
2026-08-08 11:51:14 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@
 # ==========================================================
 import streamlit as st
 from pathlib import Path
-import joblib
 import numpy as np
 import pandas as pd
 import plotly.express as px
